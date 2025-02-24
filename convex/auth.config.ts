b3d82478abdbabcf399716c0a8ad7d37cec854a2
DevUrmik <email.otherwork@gmail.com>
2025-02-24 16:25:51 +0530

    clerk provider integrated, NOTE: in auth.config.ts add a new template and change the url (till 33:16)

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://your-issuer-url.clerk.accounts.dev/", // change this to your issuer URL
+        applicationID: "convex",
+      },
+    ]
+  };
