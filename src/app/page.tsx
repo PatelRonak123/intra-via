b764aab4b757001639e76b78d567ecb4898ef626
DevUrmik <email.otherwork@gmail.com>
2025-02-27 07:04:34 +0530

    working on navbar (1:16:37)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-10">
-      <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        home page
     </div>
   );
 }
---
+++ src/components/DasboardBtn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DasboardBtn() {
+  return (
+    <div>DasboardBtn</div>
+  )
+}
+
+export default DasboardBtn
