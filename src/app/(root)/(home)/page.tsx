0995fc76d67d35ee0f7edcf940e751f6e2dd739b
DevUrmik <email.otherwork@gmail.com>
2025-03-21 23:41:04 +0530

    1:36

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,15 +1,17 @@
 "use client";
+
 import ActionCard from "@/components/ActionCard";
 import { QUICK_ACTIONS } from "@/constants";
 import { useUserRole } from "@/hooks/useUserRole";
 import { useQuery } from "convex/react";
 import { useState } from "react";
 import { api } from "../../../../convex/_generated/api";
-import { useRouter } from "next/navigation"; // changed next/user to next/navigation as i was encountering error
+import { useRouter } from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
 
 export default function Home() {
   const router = useRouter();
+
   const { isInterviewer, isCandidate, isLoading } = useUserRole();
   const interviews = useQuery(api.interviews.getMyInterviews);
   const [showModal, setShowModal] = useState(false);
@@ -37,25 +39,27 @@ export default function Home() {
 
   return (
     <div className="container max-w-7xl mx-auto p-6">
-      
+      {/* WELCOME SECTION */}
       <div className="rounded-lg bg-card p-6 border shadow-sm mb-10">
         <h1 className="text-4xl font-bold bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
           Welcome back!
         </h1>
         <p className="text-muted-foreground mt-2">
           {isInterviewer
-            ? "Manage your interviews and review candidates effectively."
-            : "Access your upcoming interviews and preparations."}
+            ? "Manage your interviews and review candidates effectively"
+            : "Access your upcoming interviews and preparations"}
         </p>
       </div>
+      
+      
       {isInterviewer ? (
         <>
           <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
-            {QUICK_ACTIONS.map((action, index) => (
+            {QUICK_ACTIONS.map((action) => (
               <ActionCard
-              key={action.title}
-              action={action}
-              onClick={() => handleQuickAction(action.title)} 
+                key={action.title}
+                action={action}
+                onClick={() => handleQuickAction(action.title)}
               />
             ))}
           </div>
@@ -69,7 +73,7 @@ export default function Home() {
         </>
       ) : (
         <>
-          <div>candidte view goes here</div>
+          <div>candidate view goes here</div>
         </>
       )}
     </div>
--- src/components/DasboardBtn.tsx
+++ src/components/DasboardBtn.tsx
@@ -1,22 +1,22 @@
 "use client";
-import  Link from 'next/link'
-import { SparkleIcon } from 'lucide-react'
-import React from 'react'
-import { Button } from './ui/button'
-import { useUserRole } from '@/hooks/useUserRole';
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { SparklesIcon } from "lucide-react";
+import { useUserRole } from "@/hooks/useUserRole";
 
 function DasboardBtn() {
-  const {isCandidate,isLoading} = useUserRole();
-  if(isCandidate || isLoading) return null
-  return(
-    <Link href= {"/dashboard"}>
-      <Button className ="gap-2 font-medium" size = {"sm"}>
-        <SparkleIcon className='size-4'/>
-          Dashboard
+  const { isCandidate, isLoading } = useUserRole();
+
+  if (isCandidate || isLoading) return null;
+
+  return (
+    <Link href={"/dashboard"}>
+      <Button className="gap-2 font-medium" size={"sm"}>
+        <SparklesIcon className="size-4" />
+        Dashboard
       </Button>
     </Link>
   );
-
 }
-
-export default DasboardBtn
+export default DasboardBtn;
