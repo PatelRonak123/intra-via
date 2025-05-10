4415144bf52a440a7b14b83eb1bb850e1d69aa90
Ronak Patel <email.otherworks@gmail.com>
2025-05-10 12:16:46 +0530

    final

--- intra-via-apk/App.tsx
+++ intra-via-apk/App.tsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { SafeAreaView, StyleSheet } from "react-native";
-// import { WebView } from "react-native-webview";
-// import Constants from "expo-constants";
-// import { StatusBar } from "expo-status-bar";
-
-// export default function App() {
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <StatusBar style="auto" />
-//       <WebView
-//         source={{ uri: "http://192.168.1.4:3000" }}
-//         style={{ flex: 1 }}
-//       />
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     marginTop: Constants.statusBarHeight,
-//   },
-// });
 
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
